b71e11cfb0265d3f4835217945889fee6da14e84
MOwais <owace.ahmed@gmail.com>
2023-01-17 21:43:27 -0500

    add client modal. InMemoryCache

--- project-mgmt-app/client/src/App.js
+++ project-mgmt-app/client/src/App.js
@@ -2,10 +2,32 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import Header from "./components/Header";
 import Clients from "./components/Clients";
+import AddClientModal from "./components/AddClientModal";
+
+//for warning - invariant.ts:42 Cache data may be lost when replacing the clients field of a Query object.
+//To address this problem (which is not a bug in Apollo Client), define a custom merge function for the Query.clients field, so InMemoryCache can safely merge these objects:
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache
 });
 
 
@@ -15,6 +37,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
